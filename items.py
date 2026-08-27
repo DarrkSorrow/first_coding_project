25091e503f328595f3ded7bf89ea026c51f143c6
DarrkSorrow <a.hett93@gmail.com>
2026-08-27 20:26:08 +0200

    2 new items added

--- items.py
+++ items.py
@@ -13,7 +13,7 @@ def random_item():
 def random_gear():
     items = (ShortSword, LongSword, ShatteredRunes,
             SimpleArmor, Boots, SimpleWarmogs, ManaMantle,
-            LifeStone, KhansHat, ElvenBoots, ShukoClaws,
+            LifeStone, KhansHat, ElvenBoots,
             RitualDagger, SoulStealer)
     item = choice(items)
     return item()
@@ -65,11 +65,13 @@ class Item:
 
 
 class Item1(Item):#HEILTRANK
+
     def __init__(self):
         super().__init__("EINFACHER HEILTRANK",
                         60, 1, 50,
                         '', simple_health_potion)
         self.dungeon = True
+
     def use_item(self, hero, enemy, combat_log):
         hero.life += self.power
         if hero.life >= hero.max_life:
@@ -85,11 +87,13 @@ class Item1(Item):#HEILTRANK
 
 
 class Item2(Item):#ODEM-ESSENZ
+
     def __init__(self):
         super().__init__("EINFACHE ODEM-ESSENZ",
                         60, 1, 50,
                         '', simple_mana_potion)
         self.dungeon = True
+
     def use_item(self, hero, enemy, combat_log):
         hero.mana += self.power
         if hero.mana >= hero.max_mana:
@@ -105,9 +109,11 @@ class Item2(Item):#ODEM-ESSENZ
 
 
 class Item3(Item):#BRANDBOMBE
+
     def __init__(self):
         super().__init__("KLEINE BRANDBOMBE",
                           20, 1, 55, '', simple_fire_bomb)
+        
     def use_item(self, hero, enemy, combat_log):
         block = (1 - enemy.reduction / 100)
         damage_taken1 = round(self.power*block)
@@ -130,13 +136,16 @@ class Item3(Item):#BRANDBOMBE
 
 
 class Item4(Item):#prakt. HEILTRANK
+
     def __init__(self):
         super().__init__("PRAKTISCHER HEILTRANK",
                           50, 2, 85, "", moderate_health_potion)
         self.dungeon = True
         self.update_symbol()
+
     def update_symbol(self):
         self.symbol = f"{self.charges}"
+
     def use_item(self, hero, enemy, combat_log):
         hero.life += self.power
         if hero.life >= hero.max_life:
@@ -156,9 +165,11 @@ class Item4(Item):#prakt. HEILTRANK
 
 class Item5(Item):#BERSERKER BLUT
     item_name = "BERSERKER-BLUT" #needed for an event
+
     def __init__(self):
         super().__init__("BERSERKER-BLUT",
                         0, 1, 100, "", berserk_blood)
+        
     def use_item(self, hero, enemy, combat_log):
         ItemBuff1(2, 10).buff(hero)
         text = f"Die Macht der Ahnen übermannt {hero.name}."
@@ -169,12 +180,15 @@ class Item5(Item):#BERSERKER BLUT
 
 
 class Item6(Item):#verb. BRANDBOMBE
+
     def __init__(self):
         super().__init__("VERBESSERTE BRANDBOMBE",
                           20, 2, 100, '', moderate_fire_bomb)
         self.update_symbol()
+
     def update_symbol(self):
         self.symbol = f"{self.charges}"
+
     def use_item(self, hero, enemy, combat_log):
         block = (1 - enemy.reduction / 100)
         damage_taken1 = round(self.power * block)
@@ -198,11 +212,13 @@ class Item6(Item):#verb. BRANDBOMBE
 
 
 class Item7(Item):#GUTE ODEM-ESSENZ
+
     def __init__(self):
         super().__init__("GUTE ODEM-ESSENZ",
                         45, 1, 85,
                         '', good_mana_potion)
         self.dungeon = True
+
     def use_item(self, hero, enemy, combat_log):
         hero.mana += self.power
         if combat_log == None:
@@ -232,13 +248,15 @@ mana_mantle = 'images/items/gear/mana_mantle.png'
 life_stone = 'images/items/gear/life_Stone.png'
 khans_hat = 'images/items/gear/khans_hat.png'
 elven_boots = 'images/items/gear/elven_boots.png'
-shuko_claws = 'images/items/gear/shuko_claws.png'
 ritual_dagger = 'images/items/gear/ritual_dagger.png'
 soul_stealer = 'images/items/gear/soul_stealer.png'
+old_pistole = 'images/items/gear/old_pistole.png'
+moon_stone = 'images/items/gear/moon_stone.png'
 #IMAGES LOADED, EVERY ITEM INSTANCE POITNS TO THESE
 
 
 class Gear:
+
     def __init__(self, name, power, xp, symbol, image):
         self.name = name
         self.power = power
@@ -268,13 +286,16 @@ class Gear:
 
 
 class ShortSword(Gear):
+
     def __init__(self):
         super().__init__("KURZSCHWERT",
                           15, 144, '', short_sword)
+        
     def equip(self, hero):
         hero.inventory.append(self)
         hero.damage += self.power / 3
         hero.speed += self.power
+
     def unequip(self, hero):
         hero.speed -= self.power
         hero.damage -= self.power / 3
@@ -282,25 +303,31 @@ class ShortSword(Gear):
 
 
 class LongSword(Gear):
+
     def __init__(self):
         super().__init__("LANGSCHWERT",
                           8, 144, '', long_sword)
+        
     def equip(self, hero):
         hero.inventory.append(self)
         hero.damage += self.power
+
     def unequip(self, hero):
         hero.damage -= self.power
         hero.inventory.remove(self)
 
 
 class ShatteredRunes(Gear):
+
     def __init__(self):
         super().__init__("VERWITTERTE RUNE",
                           10, 144, '', shattered_runes)
+        
     def equip(self, hero):
         hero.inventory.append(self)
         hero.max_mana += self.power
         hero.damage += round(self.power / 2)
+
     def unequip(self, hero):
         hero.damage -= round(self.power / 2)
         hero.max_mana -= self.power
@@ -310,36 +337,45 @@ class ShatteredRunes(Gear):
 
 
 class SimpleArmor(Gear):
+
     def __init__(self):
         super().__init__("LEDERHARNISCH",
                          7, 138, '', simple_armor)
+        
     def equip(self, hero):
         hero.inventory.append(self)
         hero.reduction += self.power
+
     def unequip(self, hero):
         hero.reduction -= self.power
         hero.inventory.remove(self)
 
 
 class Boots(Gear):
+
     def __init__(self):
         super().__init__("WANDERSTIEFEL",
                         34, 138, '', boots)
+        
     def equip(self, hero):
         hero.inventory.append(self)
         hero.speed += self.power
+
     def unequip(self, hero):
         hero.speed -= self.power
         hero.inventory.remove(self)
 
 
 class SimpleWarmogs(Gear):
+
     def __init__(self):
         super().__init__("Rüstung der Lebenskraft",
                           20, 138, '', simple_warmogs)
+        
     def equip(self, hero):
         hero.inventory.append(self)
         hero.max_life += self.power
+
     def unequip(self, hero):
         hero.max_life -= self.power
         if hero.life > hero.max_life:
@@ -348,12 +384,15 @@ class SimpleWarmogs(Gear):
 
 
 class ManaMantle(Gear):
+
     def __init__(self):
         super().__init__("Mantel der Intelligenz",
                         25, 138, '', mana_mantle)
+        
     def equip(self, hero):
         hero.inventory.append(self)
         hero.max_mana += self.power
+
     def unequip(self, hero):
         hero.max_mana -= self.power
         if hero.mana > hero.max_mana:
@@ -365,14 +404,18 @@ class ManaMantle(Gear):
 
 
 class LifeStone(Gear):
+
     def __init__(self):
         super().__init__("Stein des Lebens",
                          2, 210, '', life_stone)
         self.passive_effekt = True
+
     def equip(self, hero):
         hero.inventory.append(self)
+
     def unequip(self, hero):
         hero.inventory.remove(self)
+
     def trigger_passive(self, hero):
         hero.life += self.power
         if hero.life > hero.max_life:
@@ -380,13 +423,16 @@ class LifeStone(Gear):
 
 
 class KhansHat(Gear):
+
     def __init__(self):
-        super().__init__("Mönchs-Stirnband", 
+        super().__init__("Helm des Eroberers", 
                         4, 195, '', khans_hat)
+        
     def equip(self, hero):
         hero.inventory.append(self)
         hero.damage += self.power
         hero.critical += self.power
+
     def unequip(self, hero):
         hero.critical -= self.power
         hero.damage -= self.power
@@ -394,54 +440,117 @@ class KhansHat(Gear):
 
 
 class ElvenBoots(Gear):
+
     def __init__(self):
         super().__init__("Elfen-Schuhe", 
                         10, 185, '', elven_boots)
+        
     def equip(self, hero):
         hero.inventory.append(self)
         hero.dodge += self.power
         hero.speed += 3 * self.power
+
     def unequip(self, hero):
         hero.speed -= 3 * self.power
         hero.dodge -= self.power
         hero.inventory.append(self)
 
 
-class ShukoClaws(Gear):
-    def __init__(self):
-        super().__init__("Shuko-Krallen", 
-                        0, 180, '', shuko_claws)
-    def equip(self, hero):
-        self.power = round(hero.speed / 10)#Set power for Item
-        hero.inventory.append(self)#only once by equiping
-        hero.damage += self.power
-    def unequip(self, hero):
-        hero.damage -= self.power
-        hero.inventory.remove(self)
-
-
 class RitualDagger(Gear):
+
     def __init__(self):
         super().__init__("Ritual-Dolch",
                          2, 188, '', ritual_dagger)
         self.execution_10 = True
+
     def equip(self, hero):
         hero.inventory.append(self)
         hero.damage += self.power
+
     def unequip(self, hero):
         hero.damage -= self.power
         hero.inventory.remove(self)
 
 
 class SoulStealer(Gear):
+
     def __init__(self):
         super().__init__("Seelen-Dieb",
                         30, 200, '', soul_stealer)
         self.after_combat_effekt = True
+
     def equip(self, hero):
         hero.inventory.append(self)
+
     def unequip(self, hero):
         hero.inventory.remove(self)
+
     def after_combat(self, hero):
         if hero.mana < self.power:
             hero.mana = self.power
+
+
+class OldPistole(Gear):
+
+    def __init__(self):
+        super().__init__("Alte Pistole",
+                         15, 220, '', old_pistole)
+        
+    def equip(self, hero):
+        hero.inventory.append(self)
+        hero.damage += self.power
+        hero.speed -= self.power * 2
+
+    def unequip(self, hero):
+        hero.speed += self.power * 2
+        hero.damage -= self.power
+        hero.inventory.remove(self)
+
+
+class MoonStone(Gear):
+
+    def __init__(self):
+        super().__init__('Mondfragment',
+                         15, 210, '', moon_stone)
+        self.active = True
+        self.after_combat_effekt = True
+
+    def equip(self, hero):
+        hero.inventory.append(self)
+        hero.mental_reduction += self.power
+
+    def unequip(self, hero):
+        hero.mental_reduction -= self.power
+        hero.inventory.remove(self)
+
+    def use_item(self, hero, enemy, combat_log):
+        text = f'{hero.name} ergreift den Stein und drückt ihn fest!'
+        combat_log.add(text)
+        self.active = False
+        hero.mental_reduction -= self.power
+        hero.mana += self.power * 3
+        text = 'Der Stein hat sein Schimmern temporär verloren.'
+        combat_log.add(text)
+
+    def after_combat(self, hero):
+        if not self.active:
+            hero.mental_redcution += self.power
+            self.active = True
+
+
+#IMAGES LOADED, EVERY ITEM INSTANCE POITNS TO THESE
+shuko_claws = 'images/items/gear/shuko_claws.png'            
+#IMAGES LOADED, EVERY ITEM INSTANCE POITNS TO THESE
+            
+
+class ShukoClaws(Gear):#not in rotation
+    def __init__(self):
+        super().__init__("Shuko-Krallen", 
+                        0, 280, '', shuko_claws)
+    def equip(self, hero):
+        self.power = round(hero.speed / 9)#Set power for Item
+        hero.inventory.append(self)#only once by equiping
+        hero.damage += self.power
+    def unequip(self, hero):
+        hero.damage -= self.power
+        hero.inventory.remove(self)
